a6528a0288355dbb27f38a6af7c63fdee97db31a
Bhanu Teja K <teja8792@gmail.com>
2020-08-05 02:47:29 +0530

    Changes

--- src/views/CreateGame.vue
+++ src/views/CreateGame.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="uk-padding-small">
     <div>
+      <button class="uk-button uk-margin" @click="$router.push('/')">
+        <span uk-icon="arrow-left"></span> Back
+      </button>
       <button class="uk-button uk-button-default uk-button-large uk-width-1-1">Start</button>
     </div>
   </div>
